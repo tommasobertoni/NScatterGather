d1db7a8009e3ea92ec74fdccaff34350a4403f49
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-09-25 09:59:27 +0200

    Introduces TypeInspectorRegistryTests.

--- src/NScatterGather/Inspection/TypeInspectorRegistry.cs
+++ src/NScatterGather/Inspection/TypeInspectorRegistry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace NScatterGather.Inspection
 {
@@ -8,19 +9,25 @@ namespace NScatterGather.Inspection
         private static readonly ConcurrentDictionary<Type, TypeInspector> _registry =
             new ConcurrentDictionary<Type, TypeInspector>();
 
-        public static void Register<T>() => Register(typeof(T));
+        public static void Register<T>() =>
+            Register(typeof(T));
 
         public static void Register(Type t)
         {
             _ = _registry.TryAdd(t, new TypeInspector(t));
         }
 
-        public static TypeInspector? Of<T>() => Of(typeof(T));
+        public static TypeInspector Of<T>() =>
+            Of(typeof(T));
 
-        public static TypeInspector? Of(Type t)
+        public static TypeInspector Of(Type t)
         {
             _ = _registry.TryGetValue(t, out var inspector);
-            return inspector;
+            return inspector ??
+                throw new KeyNotFoundException($"No {nameof(TypeInspector)} for type '{t.Name}' was registered.");
         }
+
+        internal static void Clear() =>
+            _registry.Clear();
     }
 }
---
+++ tests/NScatterGather.Tests/Inspection/TypeInspectorRegistryTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace NScatterGather.Inspection
+{
+    public class TypeInspectorRegistryTests : IDisposable
+    {
+        [Fact]
+        public void Can_register_generic_type()
+        {
+            TypeInspectorRegistry.Register<object>();
+        }
+
+        [Fact]
+        public void Can_register_type()
+        {
+            TypeInspectorRegistry.Register(typeof(object));
+        }
+
+        [Fact]
+        public void Registered_type_inspector_can_be_found()
+        {
+            TypeInspectorRegistry.Register<object>();
+
+            Assert.NotNull(TypeInspectorRegistry.Of<object>());
+            Assert.NotNull(TypeInspectorRegistry.Of(typeof(object)));
+        }
+
+        [Fact]
+        public void Error_if_type_was_never_registered()
+        {
+            Assert.Throws<KeyNotFoundException>(() => TypeInspectorRegistry.Of<object>());
+            Assert.Throws<KeyNotFoundException>(() => TypeInspectorRegistry.Of(typeof(object)));
+        }
+
+        public void Dispose()
+        {
+            TypeInspectorRegistry.Clear();
+        }
+    }
+}
