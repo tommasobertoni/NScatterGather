ea94f6e4550cd3678175956b6df059b8da1adbe5
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-09-26 14:44:23 +0200

    Introduces Invocations and tests.

---
+++ tests/NScatterGather.Tests/Invocations/InvocationTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using NScatterGather.Recipients;
+using Xunit;
+
+namespace NScatterGather.Invocations
+{
+    public class InvocationTests
+    {
+        private readonly Recipient _recipient;
+
+        public InvocationTests()
+        {
+            _recipient = new Recipient(typeof(object));
+        }
+
+        [Fact]
+        public void Constructor_parameters_are_used()
+        {
+            var task = Task.FromResult(42);
+            var invocation = new Invocation<int>(_recipient, task);
+
+            Assert.NotNull(invocation.Recipient);
+            Assert.Same(_recipient, invocation.Recipient);
+            Assert.NotNull(invocation.Task);
+            Assert.Same(task, invocation.Task);
+        }
+
+        [Fact]
+        public void Error_if_recipient_parameter_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new Invocation<int>(null!, Task.FromResult(42)));
+        }
+
+        [Fact]
+        public void Error_if_task_parameter_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new Invocation<int>(_recipient, null!));
+        }
+
+        [Fact]
+        public void Can_be_deconstructed()
+        {
+            var t = Task.FromResult(42);
+            var invocation = new Invocation<int>(_recipient, t);
+
+            var (recipient, task) = invocation;
+
+            Assert.Same(_recipient, recipient);
+            Assert.Same(t, task);
+        }
+    }
+}
