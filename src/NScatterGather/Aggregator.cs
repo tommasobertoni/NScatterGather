0307c01a2a6a1ad0d08339d58815dd9d3b0176e5
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-09-28 10:44:48 +0200

    Introduces Aggregator and tests.

---
+++ src/NScatterGather/Aggregator.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+using NScatterGather.Invocations;
+using NScatterGather.Recipients;
+
+namespace NScatterGather
+{
+    public class Aggregator
+    {
+        private readonly RecipientsCollection _recipients;
+
+        public Aggregator(RecipientsCollection recipients) =>
+            _recipients = recipients;
+
+        public async Task<AggregatedResponse<object?>> Send<TRequest>(
+            TRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var recipients = _recipients.ListRecipientsAccepting<TRequest>();
+
+            var invocations = await Invoke(recipients, request, cancellationToken);
+
+            return new AggregatedResponse<object?>(invocations);
+        }
+
+        private async Task<IReadOnlyList<Invocation<object?>>> Invoke<TRequest>(
+            IReadOnlyList<Recipient> recipients,
+            TRequest request,
+            CancellationToken cancellationToken)
+        {
+            var invocations = recipients
+                .Select(p => new Invocation<object?>(p, p.Accept(request)))
+                .ToArray();
+
+            var tasks = invocations.Select(x => x.Task);
+            var allTasksCompleted = Task.WhenAll(tasks);
+
+            if (allTasksCompleted.IsCompletedSuccessfully)
+                return invocations;
+
+            using (var cts = new CancellationTokenTaskSource<object?[]>(cancellationToken))
+                await Task.WhenAny(allTasksCompleted, cts.Task);
+
+            return invocations;
+        }
+
+        public async Task<AggregatedResponse<TResponse>> Send<TRequest, TResponse>(
+            TRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var recipients = _recipients.ListRecipientsReplyingWith<TRequest, TResponse>();
+
+            var invocations = await Invoke<TRequest, TResponse>(recipients, request, cancellationToken);
+
+            return new AggregatedResponse<TResponse>(invocations);
+        }
+
+        private async Task<IReadOnlyList<Invocation<TResponse>>> Invoke<TRequest, TResponse>(
+            IReadOnlyList<Recipient> recipients,
+            TRequest request,
+            CancellationToken cancellationToken)
+        {
+            var invocations = recipients
+                .Select(p => new Invocation<TResponse>(p, p.ReplyWith<TRequest, TResponse>(request)))
+                .ToArray();
+
+            var tasks = invocations.Select(x => x.Task);
+            var allTasksCompleted = Task.WhenAll(tasks);
+
+            if (allTasksCompleted.IsCompletedSuccessfully)
+                return invocations;
+
+            using (var cts = new CancellationTokenTaskSource<TResponse[]>(cancellationToken))
+                await Task.WhenAny(allTasksCompleted, cts.Task);
+
+            return invocations;
+        }
+    }
+}
---
+++ tests/NScatterGather.Tests/AggregatorTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NScatterGather.Recipients;
+using Xunit;
+
+namespace NScatterGather
+{
+    public class AggregatorTests
+    {
+        class SomeType
+        {
+            public int Do(int n) => n * 2;
+        }
+
+        class SomeAsyncType
+        {
+            public Task<string> Do(int n) => Task.FromResult(n.ToString());
+        }
+
+        class SomeCollidingType
+        {
+            public string Do(int n) => n.ToString();
+
+            public string DoDifferently(int n) => $"{n}";
+        }
+
+        class SomeFaultingType
+        {
+            public string Fail(int n) => throw new Exception("A failure.");
+        }
+
+        private readonly Aggregator _aggregator;
+
+        public AggregatorTests()
+        {
+            var mock = new Mock<ILogger<RecipientsCollection>>();
+
+            var collection = new RecipientsCollection(mock.Object);
+            collection.Add<SomeType>();
+            collection.Add<SomeAsyncType>();
+            collection.Add<SomeCollidingType>();
+            collection.Add<SomeFaultingType>();
+
+            _aggregator = new Aggregator(collection);
+        }
+
+        [Fact(Timeout = 2_000)]
+        public async Task Sends_request_and_aggregates_responses()
+        {
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+            var result = await _aggregator.Send(42, cts.Token);
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Completed.Count);
+            Assert.Contains(typeof(SomeType), result.Completed.Select(x => x.recipientType));
+            Assert.Contains(typeof(SomeAsyncType), result.Completed.Select(x => x.recipientType));
+
+            Assert.Single(result.Faulted);
+            Assert.Contains(typeof(SomeFaultingType), result.Faulted.Select(x => x.recipientType));
+
+            Assert.Empty(result.Incomplete);
+        }
+
+        [Fact(Timeout = 2_000)]
+        public async Task Receives_expected_response_types()
+        {
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+            var result = await _aggregator.Send<int, string>(42, cts.Token);
+
+            Assert.NotNull(result);
+            Assert.Equal(1, result.Completed.Count);
+            Assert.Contains(typeof(SomeAsyncType), result.Completed.Select(x => x.recipientType));
+
+            Assert.Single(result.Faulted);
+            Assert.Contains(typeof(SomeFaultingType), result.Faulted.Select(x => x.recipientType));
+
+            Assert.Empty(result.Incomplete);
+        }
+    }
+}
