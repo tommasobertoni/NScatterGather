d5496dfca37acea1782ff71e04af5bdbe39e25f5
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-10-31 18:51:44 +0100

    Updates pack script.

--- pack/pack_nscattergather.sh
+++ pack/pack_nscattergather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-remote_pack_file_url="https://gist.githubusercontent.com/tommasobertoni/b6908c192edafe1e3a50151e0ad72ea6/raw/6873553ea16b163525fb5a5032ff8627e2775532/pack.sh"
+remote_pack_file_url="https://gist.githubusercontent.com/tommasobertoni/b6908c192edafe1e3a50151e0ad72ea6/raw/e115c1de481b98fdc981bf5ec1e8a34f0dc4369c/pack.sh"
 pack_file="pack.sh"
 
 if ! test -f "$pack_file"; then
@@ -9,4 +9,4 @@ if ! test -f "$pack_file"; then
 fi
 
 chmod a+x $pack_file
-bash $pack_file -P "../src/NScatterGather/NScatterGather.csproj" -O "tommasobertoni" -L "../LICENSE" -I "../assets/logo/nscattergather-logo-128.png"
+bash $pack_file -P "../src/NScatterGather/NScatterGather.csproj" -B -S -O "tommasobertoni" -L "../LICENSE" -I "../assets/logo/nscattergather-logo-128.png"
