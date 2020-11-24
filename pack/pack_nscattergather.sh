8dac46ce95231c4e3e4c3cb694d1c656256e9d6c
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-11-24 09:25:22 +0100

    Updates pack script.

--- pack/pack_nscattergather.sh
+++ pack/pack_nscattergather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-remote_pack_file_url="https://gist.githubusercontent.com/tommasobertoni/b6908c192edafe1e3a50151e0ad72ea6/raw/e115c1de481b98fdc981bf5ec1e8a34f0dc4369c/pack.sh"
+remote_pack_file_url="https://gist.githubusercontent.com/tommasobertoni/b6908c192edafe1e3a50151e0ad72ea6/raw/8bb95b7583bf10dc555e1f4df0feb301877e502f/pack.sh"
 pack_file="pack.sh"
 
 if ! test -f "$pack_file"; then
