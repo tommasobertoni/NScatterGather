caf44cfdd8ae7b0e102a65f830bc8ca3d2ac8747
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-12-26 13:21:37 +0100

    Fixes naming and index of results in samples.

--- samples/NScatterGather.Samples/Samples/3.InvokeAsyncMethods.cs
+++ samples/NScatterGather.Samples/Samples/3.InvokeAsyncMethods.cs
@@ -32,9 +32,9 @@ namespace NScatterGather.Samples.Samples
             // or ValueTask<TResponse> get invoked.
 
             var response2 = await aggregator.Send<long>(42);
-            var guidResults = response2.AsResultsList();
-            Console.WriteLine($"{guidResults[0]} ({guidResults[0].GetType().Name})");
-            Console.WriteLine($"{guidResults[0]} ({guidResults[1].GetType().Name})");
+            var longResults = response2.AsResultsList();
+            Console.WriteLine($"{longResults[0]} ({longResults[0].GetType().Name})");
+            Console.WriteLine($"{longResults[1]} ({longResults[1].GetType().Name})");
 
             var response3 = await aggregator.Send<string>(42);
             var stringResults = response3.AsResultsList();
