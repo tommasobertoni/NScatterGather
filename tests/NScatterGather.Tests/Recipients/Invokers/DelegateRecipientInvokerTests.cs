542e3a588666996a7fe8eb4c2ac5fc480d404f23
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2021-01-02 10:00:34 +0100

    Introduces Invokers tests.

---
+++ tests/NScatterGather.Tests/Recipients/Invokers/DelegateRecipientInvokerTests.cs
@@ -0,0 +1,21 @@
+﻿using NScatterGather.Recipients.Descriptors;
+using Xunit;
+
+namespace NScatterGather.Recipients.Invokers
+{
+    public class DelegateRecipientInvokerTests
+    {
+        [Fact]
+        public void Can_be_cloned()
+        {
+            var descriptor = new DelegateRecipientDescriptor(typeof(int), typeof(string));
+            static object? @delegate(object o) { return o.ToString(); }
+
+            var invoker = new DelegateRecipientInvoker(descriptor, @delegate);
+            var clone = invoker.Clone();
+
+            Assert.NotNull(clone);
+            Assert.IsType<DelegateRecipientInvoker>(invoker);
+        }
+    }
+}
