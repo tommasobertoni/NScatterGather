79c636bd95f1a227da49b3cfdd7a8e5ff176f3ff
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2021-01-02 10:00:16 +0100

    Introduces Descriptors tests.

---
+++ tests/NScatterGather.Tests/Recipients/Descriptors/DelegateRecipientDescriptorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+using static NScatterGather.CollisionStrategy;
+
+namespace NScatterGather.Recipients.Descriptors
+{
+    public class DelegateRecipientDescriptorTests
+    {
+        [Fact]
+        public void Can_accept_request_type()
+        {
+            var descriptor = new DelegateRecipientDescriptor(typeof(int), typeof(DateTime));
+
+            Assert.False(descriptor.CanAccept(typeof(int?), IgnoreRecipient));
+            Assert.True(descriptor.CanAccept(typeof(int), IgnoreRecipient));
+
+            var nullableDescriptor = new DelegateRecipientDescriptor(typeof(int?), typeof(DateTime?));
+
+            Assert.True(nullableDescriptor.CanAccept(typeof(int?), IgnoreRecipient));
+            Assert.True(nullableDescriptor.CanAccept(typeof(int), IgnoreRecipient));
+        }
+
+        [Fact]
+        public void Can_reply_with_response_type()
+        {
+            var descriptor = new DelegateRecipientDescriptor(typeof(int), typeof(DateTime));
+
+            Assert.False(descriptor.CanReplyWith(typeof(int?), typeof(DateTime?), IgnoreRecipient));
+            Assert.True(descriptor.CanReplyWith(typeof(int), typeof(DateTime), IgnoreRecipient));
+
+            var nullableDescriptor = new DelegateRecipientDescriptor(typeof(int?), typeof(DateTime?));
+
+            Assert.True(nullableDescriptor.CanReplyWith(typeof(int?), typeof(DateTime?), IgnoreRecipient));
+            Assert.True(nullableDescriptor.CanReplyWith(typeof(int), typeof(DateTime), IgnoreRecipient));
+        }
+    }
+}
---
+++ tests/NScatterGather.Tests/Recipients/Descriptors/TypeRecipientDescriptorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NScatterGather.Inspection;
+using Xunit;
+using static NScatterGather.CollisionStrategy;
+
+namespace NScatterGather.Recipients.Descriptors
+{
+    public class TypeRecipientDescriptorTests
+    {
+        [Fact]
+        public void Can_accept_request_type()
+        {
+            var descriptor = new TypeRecipientDescriptor(new TypeInspector(typeof(SomeNonNullableType)));
+
+            Assert.False(descriptor.CanAccept(typeof(int?), IgnoreRecipient));
+            Assert.True(descriptor.CanAccept(typeof(int), IgnoreRecipient));
+
+            var nullableDescriptor = new TypeRecipientDescriptor(new TypeInspector(typeof(SomeNullableType)));
+
+            Assert.True(nullableDescriptor.CanAccept(typeof(int?), IgnoreRecipient));
+            Assert.True(nullableDescriptor.CanAccept(typeof(int), IgnoreRecipient));
+        }
+
+        [Fact]
+        public void Can_reply_with_response_type()
+        {
+            var descriptor = new TypeRecipientDescriptor(new TypeInspector(typeof(SomeNonNullableType)));
+
+            Assert.False(descriptor.CanReplyWith(typeof(int?), typeof(DateTime?), IgnoreRecipient));
+            Assert.True(descriptor.CanReplyWith(typeof(int), typeof(DateTime), IgnoreRecipient));
+
+            var nullableDescriptor = new TypeRecipientDescriptor(new TypeInspector(typeof(SomeNullableType)));
+
+            Assert.True(nullableDescriptor.CanReplyWith(typeof(int?), typeof(DateTime?), IgnoreRecipient));
+            Assert.True(nullableDescriptor.CanReplyWith(typeof(int), typeof(DateTime?), IgnoreRecipient));
+            Assert.False(nullableDescriptor.CanReplyWith(typeof(int?), typeof(DateTime), IgnoreRecipient));
+            Assert.False(nullableDescriptor.CanReplyWith(typeof(int), typeof(DateTime), IgnoreRecipient));
+        }
+    }
+}
