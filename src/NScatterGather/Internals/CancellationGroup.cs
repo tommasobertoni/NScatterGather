02f3b59ed83e0cced8a240e62c5b64d16ad6d523
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2021-03-14 18:30:30 +0100

    Introduces CancellationGroup.
    Merges together cancellation tokens, while also enabling manual cancel.

---
+++ src/NScatterGather/Internals/CancellationGroup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace NScatterGather
+{
+    internal class CancellationGroup : IDisposable
+    {
+        private readonly CancellationTokenSource _cts;
+        private readonly CancellationTokenTaskSource<bool> _cancellationTaskSource;
+
+        public CancellationGroup(CancellationToken cancellationToken)
+            : this(new[] { cancellationToken })
+        {
+        }
+
+        public CancellationGroup(IReadOnlyList<CancellationToken> cancellationTokens)
+        {
+            _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationTokens.ToArray());
+            _cancellationTaskSource = new CancellationTokenTaskSource<bool>(_cts.Token);
+            _cancellationTaskSource.Task.ContinueWith(_ => OnCanceled?.Invoke());
+        }
+
+        public CancellationToken CancellationToken => _cts.Token;
+
+        public event Action? OnCanceled;
+
+        public void Cancel() =>
+            _cts.Cancel();
+
+        public void Dispose()
+        {
+            _cts.Dispose();
+            _cancellationTaskSource.Dispose();
+        }
+    }
+}
